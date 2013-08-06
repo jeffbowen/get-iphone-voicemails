052bb6ea0c5e06a255b38e6d664aaf9feee4c340
Jeff Bowen <jeff.d.bowen@gmail.com>
2013-08-06 09:12:50 -0700

    Use database to find and name voicemails

--- getVoicemails.sh
+++ getVoicemails.sh
@@ -6,8 +6,14 @@
 
 BACKUPS="${HOME}/Library/Application Support/MobileSync/Backup/"
 LATEST="${BACKUPS}"$(ls -t "${BACKUPS}" | head -n 1)/
-DESTINATION="${HOME}/Downloads/Voicemails/"
+DESTINATION="${HOME}/Downloads/Voicemails"
+VOICEMAIL_DIR="HomeDomain-Library/Voicemail"
+VOICEMAIL_DB_HASH=$(echo -n "${VOICEMAIL_DIR}/voicemail.db" | openssl sha1)
 
+# For future use
+CONTACTS_DB="HomeDomain-Library/AddressBook/AddressBook.sqlitedb"
+
+# Have user confirm selected backup
 MODDATE=$(stat -f "%Sm" -t "%A, %B %d, %Y at %r" "${LATEST}")
 printf "Latest iPhone backup is from $MODDATE.\n"
 read -rs -p "Copy voicemails from this backup (Y/n)?" -n 1
@@ -17,32 +23,31 @@ then
   exit 1
 fi
 
-VOICEMAILS=()
 cd "${LATEST}"
-printf "\r0 voicemails found..."
-for i in {{0..9},{a..f}}
-do
-  FILES=$(file ${i}* | grep "Adaptive Multi-Rate Codec" | cut -d: -f1)
-  for file in ${FILES}
-  do
-    VOICEMAILS+=(${file})
-  done
-  # Update status message
-  if [ -n "${FILES}" ]
-  then
-    printf "\r${#VOICEMAILS[*]} voicemails found..."
-  fi
-done
-printf "\n"
 mkdir -p "${DESTINATION}"
-for voicemail in ${VOICEMAILS[*]}
+
+# Retrieve and copy voicemails
+SQL="select ROWID, date, sender from voicemail"
+VOICEMAILS=$(sqlite3 ${VOICEMAIL_DB_HASH} "${SQL}")
+VOICEMAIL_COUNT=$(sqlite3 ${VOICEMAIL_DB_HASH} "select count(*) from voicemail")
+for voicemail in ${VOICEMAILS}
 do
-  cp ${voicemail} ${DESTINATION}${voicemail}.amr
+  ID=$(echo ${voicemail} | awk -F"|" '{ print $1 }')
+  DATE=$(echo ${voicemail} | awk -F"|" '{ print $2 }')
+  DISPLAY_DATE=$(date -j -f %s ${DATE} +"%Y-%m-%d at %I.%M.%S %p")
+  FROM=$(echo ${voicemail} | awk -F"|" '{ print $3 }')
+  if [ -z "$FROM" ]; then
+    FROM="Unknown"
+  fi
+  VM=$(echo -n "${VOICEMAIL_DIR}/${ID}.amr" | openssl sha1)
+  cp ${VM} ${DESTINATION}/"${DISPLAY_DATE}"\ from\ ${FROM}.amr
 done
+
+# Display results
 printf "Done. "
-if [ -n "${VOICEMAILS[*]}" ]
+if [ -n "${VOICEMAIL_COUNT}" ]
 then
-  printf "${#VOICEMAILS[*]} voicemails copied to \"${DESTINATION}\".\n"
+  printf "${VOICEMAIL_COUNT} voicemails copied to \"${DESTINATION}\".\n"
   open ${DESTINATION}
 else
   printf "No voicemails were found. Please make sure you do not have the\n"
